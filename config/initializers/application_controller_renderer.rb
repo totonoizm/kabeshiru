637230601126fa3fa9985893e55f2ce36bf37618
totonoizm <singer.scottie@gmail.com>
2020-12-24 03:39:15 +0000

    [add]プロフィール編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c81133e0f26052e6daa65604d230ee2ef1136bd4e57d5afe9a67e6b08c9ae949d7b90202b496a38d6790ab744b1b1178e1409ff03e2b91869fe0502776ef208c'
