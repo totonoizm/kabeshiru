1a7a5f9ec54797387a1d5e4766098b8f14d4cbc5
totonoizm <singer.scottie@gmail.com>
2020-12-24 07:21:36 +0000

    [add]ｇｙｍ登録機能

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -10,34 +10,40 @@ class GymsController < ApplicationController
     
     def create
         @gym = Gym.new(Gym_params)
-        @gym.user_id = current_user.id
         if @gym.save
-            redirect_to gym_pas(@gym.id)
+            redirect_to gym_path(@gym.id)
         else
-            @gyms = gym.all
-            render 'index'
+            render 'new'
         end
     end
     
-    
     def edit
         @gym = Gym.find(params[:id])
-        
-
     end
     
     def update
-    
+        @gym = Gym.find(params[:id])
+        if @gym.update(gym_params)
+            redirect_to gym_path(@gym.id)
+        else
+            render 'edit'
+        end
         
     end
     
     def show
-    
-        
+        @gym = Gym.find(params[:id])
     end
     
     def destroy
         
     end
     
+    
+    private
+    def gym_params
+        params.require(:gym).permit(:name, :introduction, :tel, :gym_image, :state,
+                                    :address, :postcode, :url, :open_time, :price, :holiday,
+                                    :access, :latitude, :longitude)
+    end
 end
