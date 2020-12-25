37c2ff1dec03d1007319c63bbf5491463b670108
totonoizm <singer.scottie@gmail.com>
2020-12-25 10:42:07 +0000

    [add]コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+    def create
+        @gym = Gym.find(params[:gym_id])
+        @comment = Comment.new(comment_params)
+        @comment.gym_id = @gym.id
+        @comment.user_id = current_user.id
+        unless @comment.save
+            render 'gyms/show'
+        end
+    end
+    
+    def destroy
+    end
+    
+    private
+    def comment_params
+        params.require(:comment).permit(:comment)
+    end
 end
