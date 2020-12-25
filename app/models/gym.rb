406522abf512b08161a354835ab221b5be5fb945
totonoizm <singer.scottie@gmail.com>
2020-12-25 09:20:35 +0000

    ルーティング設計

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,6 @@
 class Gym < ApplicationRecord
+    has_many :comments, dependent: :destroy
     attachment :gym_image
     attachment :profile_image
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :post_image
   attachment :profile_image
+  has_many :comments, dependent: :destroy
+  has_many :gyms #ジム登録情報も紐付け　誰が更新、追加したかわかるように。退会しても消えないようにオプションは無し
+  
 end
