1007cbfb982cca7d9d25131240fee0c2ef416cd3
totonoizm <singer.scottie@gmail.com>
2020-12-26 15:16:25 +0000

    [add]ジム検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :gym_search
   
   def after_sign_in_path_for(resource)
     user_path  (current_user.id)
@@ -7,6 +8,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+  
+  def gym_search #ransack検索メソッド
+    @search = Gym.ransack(params[:q])
+    @search_gyms = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(10) #ジム検索一覧
   end
   
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'users#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   resources :gyms do
+    collection do
+      get :search
+    end
     resources :comments
   end
-  resources :users
 end
