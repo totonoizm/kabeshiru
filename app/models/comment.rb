b2435a6af9c56e578754c024280d369b0981ad3f
totonoizm <singer.scottie@gmail.com>
2020-12-26 06:26:04 +0000

    [add]一人のユーザーがそれぞれの店舗に対して何件コメントしたかを表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :gym
+    
+    #クラスメソッド　一つのジムに対して何回コメントしたかを取得
+    def users_comment_count
+       user.comments.where(gym_id:self.gym_id).count
+    end
 end
