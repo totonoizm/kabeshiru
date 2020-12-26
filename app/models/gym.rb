03060597fee214d132e53b13c60b921651e71771
totonoizm <singer.scottie@gmail.com>
2020-12-26 07:39:08 +0000

    [fix]ジム編集修正

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,6 +1,6 @@
 class Gym < ApplicationRecord
     has_many :comments, dependent: :destroy
-    belongs_to :user
+    belongs_to :user, optional: true
     attachment :gym_image
     attachment :profile_image
 end
