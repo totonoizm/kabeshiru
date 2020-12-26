89a24c61f64650ae7bee572137cfab5815807b67
totonoizm <singer.scottie@gmail.com>
2020-12-26 07:39:08 +0000

    [fix]ジム編集修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :gym
     
+    
     #クラスメソッド　一つのジムに対して何回コメントしたかを取得
     def users_comment_count
        user.comments.where(gym_id:self.gym_id).count
