a4185ada2a52e97e6c04260bcf9b2bf596cc271c
totonoizm <singer.scottie@gmail.com>
2020-12-27 14:02:00 +0000

    [fix]コメント編集機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'gyms#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :users
+  #   resources :comments
+  # end
+  resources :comments
+
   resources :gyms do
     collection do
       get :search
