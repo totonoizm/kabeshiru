b4279a63062c7d05076fbaaa17510146abba097e
totonoizm <singer.scottie@gmail.com>
2020-12-28 13:29:41 +0000

    [add]ブックマークボタン非同期

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -4,13 +4,11 @@ class ClipsController < ApplicationController
         @gym = Gym.find(params[:gym_id])
         clip = @gym.clips.new(user_id: current_user.id)
         clip.save
-        redirect_to request.referer
     end
     
     def destroy
         @gym = Gym.find(params[:gym_id])
         clip = @gym.clips.find_by(user_id: current_user.id)
         clip.destroy
-        redirect_to request.referer
     end
 end
