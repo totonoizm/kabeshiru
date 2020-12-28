6c1c7bbebafd8b5b8d8630737ca295ae96ffc8f6
totonoizm <singer.scottie@gmail.com>
2020-12-28 08:57:26 +0000

    [add]ジムブックマーク

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_020900) do
+ActiveRecord::Schema.define(version: 2020_12_28_070759) do
+
+  create_table "clips", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "gym_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
