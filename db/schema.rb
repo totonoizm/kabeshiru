bdd7706a524082744721f744354f5f332a41f726
totonoizm <singer.scottie@gmail.com>
2020-12-28 02:25:10 +0000

    [fix]Gymsmigrationデータ型変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_042418) do
+ActiveRecord::Schema.define(version: 2020_12_28_020900) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2020_12_27_042418) do
     t.string "price"
     t.string "holiday"
     t.string "access"
-    t.string "latitude"
-    t.string "longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
