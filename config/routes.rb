08a659fa46d9ea77056b8b8ac5a137ce1263b755
totonoizm <singer.scottie@gmail.com>
2021-01-07 06:16:31 +0000

    [fix]layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :users do
+  resources :users, only: [:show, :edit, :update] do
     member do
       get "quit"
       patch "withdraw"
