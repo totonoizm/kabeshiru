16a599f65870af52ef35c2095686a3d1c80a1e67
totonoizm <singer.scottie@gmail.com>
2021-01-08 07:49:17 +0000

    [fix]header,footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :gym_search
   
   def after_sign_in_path_for(resource)
-    user_path  (current_user.id)
+    root_path  (current_user.id)
   end
 
   def after_sign_out_path_for(resource)
