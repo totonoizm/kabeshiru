ebb66b8ec9c6885ba947b4015be69aca47bb66a7
totonoizm <singer.scottie@gmail.com>
2021-01-11 12:06:55 +0000

    [fix]rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe 'バリデーションのテスト' do
-    let(:user) { build(:user) }
-    subject { test_user.valid? }
-    context 'nameカラム' do
-      let(:test_user) { user }
-      it '空欄でないこと' do
-        test_user.name = ''
-        is_expected.to eq false;
-      end
-      it '2文字以上であること' do
-        test_user.name = Faker::Lorem.characters(number:1)
-        is_expected.to eq false;
-      end
-      it '20文字以下であること' do
-        test_user.name = Faker::Lorem.characters(number:21)
-        is_expected.to eq false;
-      end
+  describe 'バリデーションのテスト' do
+  let(:user) { build(:user) }
+  subject { test_user.valid? }
+  context 'nameカラム' do
+    let(:test_user) { user }
+    it '空欄でないこと' do
+      test_user.name = ''
+      is_expected.to eq false;
     end
-  it "ユーザー名、メール、パスワードがある場合有効である" do
-    user = User.new(
-      name: "yasu",
-      email:"test@test",
-      password: "password"
-      )
-      expect(user).to be_valid
+    it '1文字以上であること' do
+      test_user.name = Faker::Lorem.characters(number:0)
+      is_expected.to eq false;
+    end
+    it '30文字以下であること' do
+      test_user.name = Faker::Lorem.characters(number:31)
+      is_expected.to eq false;
+    end
+    it "登録済のユーザーネームと重複していないこと" do
+      test_user.name = Faker:: Lorem.characters(number:0)
+      is_expected.to eq false;
+    end
   end
-  it "ユーザー名がない場合、無効である"
-  it "メールがない場合無効である"
+    it "NGWORDが入っていないこと" do
+    　test_user.name = Faker::Lorem.characters() 
+    end
+  context 'emailkaramu'  
+  it "メールがない場合無効であcる"
   it "重複したメールの場合無効である"
   it "パスワードがない場合無効である"
 end  
