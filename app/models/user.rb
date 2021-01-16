dbaa7f53e3be812a2bfef203ff84f8129911e676
totonoizm <singer.scottie@gmail.com>
2021-01-16 10:40:26 +0000

    [fix]guestusername

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
     end
   end
   
