3f9e406b8b6f650f4c7393960bf922759d5cb0b4
totonoizm <singer.scottie@gmail.com>
2021-02-21 14:30:28 +0000

    [add]follow ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,14 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
       
   def create
+    @user = User.find(params[:id])
     current_user.follow(params[:id])
-    redirect_to request.referer
+    render 'create'
   end
   
   def destroy
+    @user = User.find(params[:id])
     current_user.unfollow(params[:id])
-    redirect_to request.referer
+    render 'destroy'
   end
 end
