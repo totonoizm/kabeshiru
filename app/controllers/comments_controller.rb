32becbb5f8329ec0a9746f64168b18508c219365
totonoizm <singer.scottie@gmail.com>
2021-02-24 16:00:41 +0000

    [add]日付変更機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,6 +55,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :comment_image)
+    params.require(:comment).permit(:comment, :comment_image, :created_at)
   end
 end
