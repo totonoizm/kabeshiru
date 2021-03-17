5a73e0148c24f90a53044852fb6e871f33adf22c
totonoizm <singer.scottie@gmail.com>
2021-03-17 12:31:12 +0000

    [check]deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,13 +40,15 @@ jQuery(document).on('turbolinks:load', function(){
         reader.readAsDataURL(e.target.files[0]);
     });
     
-$('a[href^="#"]').click(function() {
-  var speed = 400;
-  var href = $(this).attr("href");
-  var target = $(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top;
-  $('body,html').animate({
-    scrollTop: position
-  }, speed, 'swing');
-  return false;
-});});
+// $('a[href^="#"]').click(function() {
+//   var speed = 400;
+//   var href = $(this).attr("href");
+//   var target = $(href == "#" || href == "" ? 'html' : href);
+//   var position = target.offset().top;
+//   $('body,html').animate({
+//     scrollTop: position
+//   }, speed, 'swing');
+//   return false;
+// });
+    
+});
