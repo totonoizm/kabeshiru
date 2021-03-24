103c09f1b778df55e82631898b0bdbf9eb4fde78
totonoizm <singer.scottie@gmail.com>
2021-03-24 18:03:25 +0000

    [fix]chat view

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,23 +1,27 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_user
-  
-  
+
+
     def show
         @user = User.find(params[:id]) # メッセージ相手を取得
-        rooms = current_user.user_rooms.pluck(:room_id)
-        user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
-        
-        if user_rooms.nil?
-            @room = Room.new
-            @room.save
-            UserRoom.create(user_id: current_user.id, room_id: @room.id)
-            UserRoom.create(user_id: @user.id, room_id: @room.id)
+        if @user.id != current_user.id # chatroomへの制限
+            rooms = current_user.user_rooms.pluck(:room_id)
+            user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+            
+            if user_rooms.nil?
+                @room = Room.new
+                @room.save
+                UserRoom.create(user_id: current_user.id, room_id: @room.id)
+                UserRoom.create(user_id: @user.id, room_id: @room.id)
+            else
+                @room = user_rooms.room
+            end
+            @chats = @room.chats
+            @chat = Chat.new(room_id: @room.id)
         else
-            @room = user_rooms.room
+            redirect_to root_path
         end
-        @chats = @room.chats
-        @chat = Chat.new(room_id: @room.id)
     end
     
     def create
