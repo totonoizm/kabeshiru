a7772b56d88574017280ee59ccd0c26d42c08a6a
totonoizm <singer.scottie@gmail.com>
2021-04-14 13:36:52 +0000

    [fix]mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -11,6 +10,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
